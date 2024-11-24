1b4b3685d0ee34d91396fa65597b02080362fcf2
Narehate <matipietka@gmail.com>
2024-11-24 20:34:55 +0100

    Refactor

--- main.py
+++ main.py
@@ -1,97 +1,126 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import Tuple, List
-
+from typing import Tuple, List, Optional
+#https://homepages.inf.ed.ac.uk/rbf/HIPR2/fourier.htm
 image_filename = "test.bmp"
 
-class ImageToFFTConverter:
-    def _to_fft(self, channel : np.ndarray) -> np.ndarray:
+class Image:
+    def __init__(self, path : Optional[str] = None, fft : Optional["FFT"] = None):
+        if path != None:
+            self._image = plt.imread(path)
+        elif fft != None:
+            image_y_size = len(fft.get_red())
+            image_x_size = len(fft.get_red()[0])
+            image_channel_count = 3
+            self._image = np.ndarray(
+                (image_y_size,
+                image_x_size,
+                image_channel_count),
+                np.uint8)
+
+            self._image[:, :, 0] = self._fft_to_channel(fft.get_red())
+            self._image[:, :, 1] = self._fft_to_channel(fft.get_blue())
+            self._image[:, :, 2] = self._fft_to_channel(fft.get_green())
+        else:
+            raise ValueError("Missing fft or path to image !")
+
+    def get_red_channel(self) -> np.ndarray:
+        return self._image[:, :, 0]
+
+    def get_blue_channel(self) -> np.ndarray:
+        return self._image[:, :, 1]
+
+    def get_green_channel(self) -> np.ndarray:
+        return self._image[:, :, 2]
+
+    def get_all_channels(self) -> np.ndarray:
+        return self._image
+
+    def _fft_to_channel(self, fft : np.ndarray) -> np.ndarray:
+        return abs(np.fft.ifft2(fft))
+
+class FFT:
+    def __init__(self, image : Image):
+        self._red_fft = self._channel_to_fft(image.get_red_channel())
+        self._blue_fft = self._channel_to_fft(image.get_blue_channel())
+        self._green_fft = self._channel_to_fft(image.get_green_channel())
+
+    def get_red(self):
+        return self._red_fft
+
+    def get_blue(self):
+        return self._blue_fft
+
+    def get_green(self):
+        return self._green_fft
+
+    def _channel_to_fft(self, channel : np.ndarray) -> np.ndarray:
         return np.fft.fftshift(np.fft.fft2(channel))
 
-    def __init__(self, image : np.ndarray) -> None:
-        self.red_fft = self._to_fft(image[:, :, 0])
-        self.blue_fft = self._to_fft(image[:, :, 1])
-        self.green_fft = self._to_fft(image[:, :, 2])
+image = Image(path=image_filename)
 
-class FFTToImageConverter:
-    def _to_channel(self, fft : np.ndarray) -> np.ndarray:
-        return np.fft.ifft2(fft)
-
-    def _channels_to_image(self) -> np.ndarray:
-        image_y_size = len(self.red_channel)
-        image_x_size = len(self.red_channel[0])
-        image_channel_count = 3
-        image = np.ndarray(
-            (image_y_size,
-             image_x_size,
-             image_channel_count),
-             np.uint8)
-        
-        image[:, :, 0] = abs(self.red_channel)
-        image[:, :, 1] = abs(self.blue_channel)
-        image[:, :, 2] = abs(self.green_channel)
-        return image
-
-    def __init__(
-            self,
-            red : np.ndarray,
-            blue : np.ndarray,
-            green : np.ndarray
-    ) -> None:
-        self.red_channel = self._to_channel(red)
-        self.blue_channel = self._to_channel(blue)
-        self.green_channel = self._to_channel(green)
-        self.image = self._channels_to_image()
-
-image = plt.imread(image_filename)
-
-plt.subplot(532)
+plt.subplot(632)
 plt.tight_layout()
 plt.title("Orginal image")
 plt.axis("off")
-plt.imshow(image)
-img_to_fft_conv = ImageToFFTConverter(image)
+plt.imshow(image.get_all_channels())
 
 plt.set_cmap("gray")
-plt.subplot(534)
+plt.subplot(634)
+plt.title("Red channel")
+plt.axis("off")
+plt.imshow(image.get_red_channel())
+
+plt.subplot(635)
+plt.title("Blue channel")
+plt.axis("off")
+plt.imshow(image.get_blue_channel())
+
+plt.subplot(636)
+plt.title("Green channel")
+plt.axis("off")
+plt.imshow(image.get_green_channel())
+
+fft = FFT(image)
+
+plt.set_cmap("gray")
+plt.subplot(637)
 plt.title("FFT of red channel")
 plt.axis("off")
-plt.imshow(np.log(abs(img_to_fft_conv.red_fft)))
+plt.imshow(np.log(abs(fft.get_red())))
 
-plt.subplot(535)
+plt.subplot(638)
 plt.title("FFT of blue channel")
 plt.axis("off")
-plt.imshow(np.log(abs(img_to_fft_conv.blue_fft)))
+plt.imshow(np.log(abs(fft.get_blue())))
 
-plt.subplot(536)
+plt.subplot(639)
 plt.title("FFT of green channel")
 plt.axis("off")
-plt.imshow(np.log(abs(img_to_fft_conv.green_fft)))
+plt.imshow(np.log(abs(fft.get_green())))
 
-fft_to_img_conv = FFTToImageConverter(
-    img_to_fft_conv.red_fft,
-    img_to_fft_conv.blue_fft,
-    img_to_fft_conv.green_fft)
+reconstructed_image = Image(fft=fft)
 
-plt.subplot(537)
-plt.title("Red channel from FFT")
+plt.subplot(6, 3, 10)
+plt.title("Recreated red channel")
 plt.axis("off")
-plt.imshow(abs(fft_to_img_conv.red_channel))
+plt.imshow(reconstructed_image.get_red_channel())
 
-plt.subplot(538)
-plt.title("Blue channel from FFT")
+plt.subplot(6, 3, 11)
+plt.title("Recreated blue channel")
 plt.axis("off")
-plt.imshow(abs(fft_to_img_conv.blue_channel))
+plt.imshow(reconstructed_image.get_blue_channel())
 
-plt.subplot(539)
-plt.title("Green channel from FFT")
+plt.subplot(6, 3, 12)
+plt.title("Recreated green channel")
 plt.axis("off")
-plt.imshow(abs(fft_to_img_conv.green_channel))
+plt.imshow(reconstructed_image.get_green_channel())
 
-plt.subplot(5,3,11)
+plt.subplot(6,3,14)
 plt.title("Recreated image")
 plt.axis("off")
-plt.imshow(abs(fft_to_img_conv.image))
+plt.imshow(reconstructed_image.get_all_channels())
 plt.set_cmap("viridis")
 
 plt.show()
+#https://homepages.inf.ed.ac.uk/rbf/HIPR2/fourier.htm
