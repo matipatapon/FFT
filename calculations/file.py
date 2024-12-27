b72fbe8b4f278da4c7be1a48b5aa4af7221718fa
Narehate <matipietka@gmail.com>
2024-12-27 16:03:37 +0100

    Dumb compression to int16

--- calculations/file.py
+++ calculations/file.py
@@ -1,6 +1,23 @@
 import numpy as np
 from calculations.fft import FFT
 
+def _float64_to_int16(flaot64: np.float64):
+    roundedFloat64 = round(flaot64)
+    if np.iinfo('int32').max < roundedFloat64:
+        print(f"{roundedFloat64} -> {np.iinfo('int32').max}")
+        roundedFloat64 = np.iinfo('int32').max
+    if np.iinfo('int32').min > roundedFloat64:
+        roundedFloat64 = np.iinfo('int32').min
+    roundedFloat64 = np.int32(roundedFloat64)
+    roundedFloat64 >>= 16
+
+    return np.int16(roundedFloat64)
+
+def _int16_to_float64(int16: np.int16):
+    int32 = np.int32(int16)
+    int32 <<= 16
+    return np.float64(int32)
+
 def fft_to_file(path: str, fft: FFT) -> None:
     width = np.uint16(fft.get_width())
     height = np.uint16(fft.get_height())
@@ -11,37 +28,38 @@ def fft_to_file(path: str, fft: FFT) -> None:
     red = fft.get_red()
     green = fft.get_green()
     blue = fft.get_blue()
-    z = np.uint8(0)
+    how_many_zeroes_ahead = 0
 
     for x in range(0, height):
         for y in range(0, width):
             for channel in (red[x][y], green[x][y], blue[x][y]):
-                if channel == 0 and z != np.iinfo('uint8').max and (x != height - 1 or y != width - 1):
-                    z+=1
+                if channel == 0 and how_many_zeroes_ahead != np.iinfo('uint8').max and (x != height - 1 or y != width - 1):
+                    how_many_zeroes_ahead+=1
                     continue
-                f.write(channel.real.tobytes())
-                f.write(channel.imag.tobytes())
-                f.write(z.tobytes())
-                z = np.uint8(0)
+                f.write(_float64_to_int16(channel.real).tobytes())
+                f.write(_float64_to_int16(channel.imag).tobytes())
+                f.write(np.uint8(how_many_zeroes_ahead).tobytes())
+                how_many_zeroes_ahead = 0
 
 def file_to_fft(path: str) -> FFT:
     f = open(path, "rb")
-    width = int(np.frombuffer(f.read1(2), np.uint16))
-    height = int(np.frombuffer(f.read1(2), np.uint16))
 
-    r = np.ndarray(shape=(height, width), dtype=np.complex128)
-    g = np.ndarray(shape=(height, width), dtype=np.complex128)
-    b = np.ndarray(shape=(height, width), dtype=np.complex128)
-    skip = 0
+    width = np.frombuffer(f.read1(2), np.uint16, 1)[0]
+    height = np.frombuffer(f.read1(2), np.uint16, 1)[0]
+    red = np.ndarray(shape=(height, width), dtype=np.complex128)
+    green = np.ndarray(shape=(height, width), dtype=np.complex128)
+    blue = np.ndarray(shape=(height, width), dtype=np.complex128)
+    how_many_zeroes_ahead = 0
+
     for x in range(0, height):
         for y in range(0, width):
-            for channel in (r, g, b):
-                if skip != 0:
-                    channel[x][y] = np.complex128(0,0)
-                    skip-=1
+            for channel in (red, green, blue):
+                if how_many_zeroes_ahead != 0:
+                    channel[x][y] = np.complex128(0, 0)
+                    how_many_zeroes_ahead-=1
                     continue
-                channel[x][y] = np.complex128(np.frombuffer(f.read1(8), np.float64, 1), np.frombuffer(f.read1(8), np.float64, 1))
-                skip = np.frombuffer(f.read1(1), np.uint8, 1)[0]
-                print(skip)
-
-    return FFT(r, b, g)
+                real = _int16_to_float64(np.frombuffer(f.read1(2), np.int16, 1)[0])
+                imag = _int16_to_float64(np.frombuffer(f.read1(2), np.int16, 1)[0])
+                channel[x][y] = np.complex128(real, imag)
+                how_many_zeroes_ahead = np.frombuffer(f.read1(1), np.uint8, 1)[0]
+    return FFT(red, blue, green)
