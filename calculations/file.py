1ed1160bbd7f5c977b0b13e7f6b7d2a6ca6fe211
Narehate <matipietka@gmail.com>
2024-12-27 14:35:36 +0100

    Skipping zeroes

--- calculations/file.py
+++ calculations/file.py
@@ -1,44 +1,47 @@
-from numpy import float64, float16, uint32, uint16, frombuffer, finfo, ndarray, complex128
+import numpy as np
 from calculations.fft import FFT
 
-def convert_float64_to_float16(x : float64) -> float16:
-    if x > finfo('float16').max:
-        return finfo('float16').max
-    if x < finfo('float16').min:
-        return finfo('float16').min
-    return float16(x)
-
 def fft_to_file(path: str, fft: FFT) -> None:
-    width = uint16(fft.get_width())
-    height = uint16(fft.get_height())
+    width = np.uint16(fft.get_width())
+    height = np.uint16(fft.get_height())
     f = open(path, "wb")
     f.write(width.tobytes())
     f.write(height.tobytes())
 
-    r = fft.get_red()
-    g = fft.get_green()
-    b = fft.get_blue()
-    z = 0
+    red = fft.get_red()
+    green = fft.get_green()
+    blue = fft.get_blue()
+    z = np.uint8(0)
+
     for x in range(0, height):
         for y in range(0, width):
-            for channel in (r[x][y], g[x][y], b[x][y]):
+            for channel in (red[x][y], green[x][y], blue[x][y]):
+                if channel == 0 and z != np.iinfo('uint8').max and (x != height - 1 or y != width - 1):
+                    z+=1
+                    continue
                 f.write(channel.real.tobytes())
                 f.write(channel.imag.tobytes())
+                f.write(z.tobytes())
+                z = np.uint8(0)
 
 def file_to_fft(path: str) -> FFT:
     f = open(path, "rb")
-    width = int(frombuffer(f.read1(2), uint16))
-    height = int(frombuffer(f.read1(2), uint16))
-    print(f"width: {width}, height: {height}")
+    width = int(np.frombuffer(f.read1(2), np.uint16))
+    height = int(np.frombuffer(f.read1(2), np.uint16))
 
-    r = ndarray(shape=(height, width), dtype=complex128)
-    g = ndarray(shape=(height, width), dtype=complex128)
-    b = ndarray(shape=(height, width), dtype=complex128)
-
+    r = np.ndarray(shape=(height, width), dtype=np.complex128)
+    g = np.ndarray(shape=(height, width), dtype=np.complex128)
+    b = np.ndarray(shape=(height, width), dtype=np.complex128)
+    skip = 0
     for x in range(0, height):
         for y in range(0, width):
             for channel in (r, g, b):
-                channel[x][y] = complex(frombuffer(f.read1(8), float64), frombuffer(f.read1(8), float64))
+                if skip != 0:
+                    channel[x][y] = np.complex128(0,0)
+                    skip-=1
+                    continue
+                channel[x][y] = np.complex128(np.frombuffer(f.read1(8), np.float64, 1), np.frombuffer(f.read1(8), np.float64, 1))
+                skip = np.frombuffer(f.read1(1), np.uint8, 1)[0]
+                print(skip)
 
     return FFT(r, b, g)
-
